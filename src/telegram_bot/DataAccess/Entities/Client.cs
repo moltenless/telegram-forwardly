7b459142fe608f2ed7623d76d768edd78188f597
Serhii Turlak <turlakserhii@gmail.com>
2025-05-19 10:51:02 +0300

    Add clients entity

---
+++ src/telegram_bot/DataAccess/Entities/Client.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramForwardly.DataAccess.Entities
+{
+    [Table("clients")]
+    internal class Client
+    {
+        [Column("user_id", TypeName = "bigint")]
+        [Key]
+        public long UserId { get; set; }
+
+        [Column("current_state_id", TypeName = "int")]
+        [ForeignKey(nameof(CurrentState))]
+        public int? CurrentStateId { get; set; }
+        public virtual ClientCurrentState CurrentState { get; set; }
+
+        [Column("api_id", TypeName = "varchar(20)")]
+        public string? ApiId { get; set; }
+
+        [Column("api_hash", TypeName = "varchar(64)")]
+        public string? ApiHash { get; set; }
+
+        [Column("session_sting", TypeName = "varchar(600)")]
+        public string? SessionString { get; set; }
+
+        [Column("phone", TypeName = "varchar(20)")]
+        public string? Phone { get; set; }
+
+        [Column("verification_code", TypeName = "varchar(16)")]
+        public string? VerificationCode { get; set; }
+
+        [Column("password", TypeName = "nvarchar(100)")]
+        public string? Password { get; set; }
+
+        [Column("registration_datetime", TypeName = "datetime")]
+        public DateTime? RegistrationDataTime { get; set; }
+
+        [Column("username", TypeName = "varchar(32)")]
+        public string? UserName { get; set; }
+
+        [Column("first_name", TypeName = "nvarchar(64)")]
+        public string? FirstName { get; set; }
+
+        [Column("forum_supergroup_id", TypeName = "bigint")]
+        public long? ForumSupergroupId { get; set; }
+
+        [Column("logging_topic_enabled", TypeName = "bit")]
+        public bool? LoggingTopicEnabled { get; set; }
+
+        [Column("topic_grouping_type_id", TypeName = "int")]
+        [ForeignKey(nameof(TopicGroupingType))]
+        public int? TopicGroupingTypeId { get; set; }
+        public virtual TopicGroupingType TopicGroupingType { get; set; }
+
+        [Column("forwardly_enabled", TypeName = "bit")]
+        public bool? ForwardlyEnabled { get; set; }
+
+        [Column("all_chats_filtering_enabled", TypeName = "bit")]
+        public bool? AllChatsFilteringEnabled { get; set; } 
+    }
+}
