117be5e0de360dd0afdec02e6fec29a7680f7d21
Serhii Turlak <turlakserhii@gmail.com>
2025-05-19 11:46:47 +0300

    Add chat entity

---
+++ src/telegram_bot/DataAccess/Entities/Chat.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TelegramForwardly.DataAccess.Entities
+{
+    [Table("chats")]
+    internal class Chat
+    {
+        [Column("db_id", TypeName = "int")]
+        [Key]
+        public int Id { get; set; }
+
+        [Column("user_id", TypeName = "bigint")]
+        [ForeignKey(nameof(Client))]
+        [Required]
+        public long UserId { get; set; }
+        public virtual Client Client { get; set; } = null!;
+
+        [Column("tg_id", TypeName = "bigint")]
+        [Required]
+        public long ChatId { get; set; }
+
+        [Column("type_id")]
+        [ForeignKey(nameof(Type))]
+        [Required]
+        public int TypeId { get; set; }
+        public ChatType Type { get; set; } = null!;
+    }
+}
