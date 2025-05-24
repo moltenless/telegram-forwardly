ac382cf6ac6875462863c10211c553bbd31224e0
Serhii Turlak <turlakserhii@gmail.com>
2025-05-25 02:33:49 +0300

    Set environment variables and config file for further retreiving

--- src/telegram_bot/WebApi/Models/Dtos/TelegramBotOptions.cs
+++ src/telegram_bot/WebApi/Models/Dtos/TelegramBotOptions.cs
@@ -2,9 +2,9 @@
 {
     public class TelegramBotOptions
     {
-        public string BotToken { get; set; } = string.Empty;
-        public string WebhookUrl { get; set; } = string.Empty;
-        public bool UseWebhook { get; set; } = false;
-        public string UserbotApiBaseUrl { get; set; } = string.Empty;
+        public string BotToken { get; set; } = null!;
+        public bool? UseWebhook { get; set; } = null!;
+        public string WebhookUrl { get; set; } = null!;
+        public string UserbotApiBaseUrl { get; set; } = null!;
     }
 }
