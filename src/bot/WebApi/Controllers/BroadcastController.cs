896be235780f41cb0441e96a8a8bc7e9a4620d3f
Serhii Turlak <turlakserhii@gmail.com>
2025-07-22 15:13:52 +0300

    Open secure endpoint for broadcasting notification to all bot users while server is on run

---
+++ src/bot/WebApi/Controllers/BroadcastController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using TelegramForwardly.WebApi.Models.Dtos;
+using TelegramForwardly.WebApi.Models.Requests;
+using TelegramForwardly.WebApi.Services.Bot;
+using TelegramForwardly.WebApi.Services.Interfaces;
+
+namespace TelegramForwardly.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BroadcastController(
+        IUserService userService,
+        ITelegramBotClient botClient,
+        IOptions<TelegramConfig> config,
+        ILogger<BroadcastController> logger) : ControllerBase
+    {
+        private readonly string apiKey = config.Value.ApiKey;
+
+        [HttpPost("notify")]
+        public async Task<IActionResult> BroadcastNotificationAsync(
+            [FromHeader(Name = "X-Api-Key")] string apiKey,
+            [FromBody] BroadcastNotificationRequest request)
+        {
+            if (string.IsNullOrEmpty(apiKey) || apiKey != this.apiKey)
+                return Unauthorized("Invalid or missing API key");
+
+            if (request is null) return BadRequest("Text is missing");
+
+            try
+            {
+                var usersIds = (await userService.GetAllUsersAsync())
+                    .Select(u => u.TelegramUserId);
+
+                foreach (var id in usersIds)
+                {
+                    await BotHelper.SendTextMessageAsync(
+                        id, request.Text, botClient, logger,
+                        CancellationToken.None, null,
+                        request.UseMarkdown ? ParseMode.MarkdownV2 : ParseMode.None);
+                    logger.LogInformation("Broadcasted user {Id} with notification: {Text}", id, request.Text[0..10]); // can cause an error if notification is smaller
+                }
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error broadcasting the notification");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
---
+++ src/bot/WebApi/Models/Requests/BroadcastNotificationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramForwardly.WebApi.Models.Requests
+{
+    public class BroadcastNotificationRequest
+    {
+        [Required]
+        public required string Text { get; set; }
+        public bool UseMarkdown { get; set; } = false;
+    }
+}
